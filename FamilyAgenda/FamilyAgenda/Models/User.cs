d3180c1731bd1fb1da02076d6a542b306eb244c5
pskyv <pskyvalidas@gmail.com>
2020-02-25 00:29:02 +0200

    feat: Add helper classes

---
+++ FamilyAgenda/FamilyAgenda/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FamilyAgenda.Models
+{
+    public class User
+    {
+        [JsonIgnore]
+        public string UserId { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("profile_photo")]
+        public ImageSource ProfilePhoto { get; set; }
+    }
+}
---
+++ FamilyAgenda/FamilyAgenda/Utils/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyAgenda.Utils
+{
+    public static class Constants
+    {
+        #region firebase
+        public const string FirebaseDataBaseUrl = "https://familyagenda-9dcc8.firebaseio.com/";
+        #endregion
+
+        public const string SyncfusionLicenseKey = "MjI0MzBAMzEzNjJlMzIyZTMwTzdkSW5TOXhCeVR6WUVqOStUZ1c2c3FrK2RzZDJ4V01ENTN4dGpkK0dWdz0=;" +
+                                                   "MjI0MzFAMzEzNjJlMzIyZTMwQzE4bGx6Wmk0ejdzYlRZUEN5bUljRVltS2lLRU40OHhSMnZIRWFZLzR4bz0=;" +
+                                                   "MjI0MzJAMzEzNjJlMzIyZTMwZFMzdWF2ZVpHd3Bhak1JdWtweGJxSHBlZmJwSi8zQjhudkRxcjZVcEFBMD0=;" +
+                                                   "MjI0MzNAMzEzNjJlMzIyZTMwbktkMVlPaWVDTGtkREFuVWxjT01lMGJaZ1JjRHp1S0gyS2ZVWCtjTnBZTT0=;" +
+                                                   "MjI0MzRAMzEzNjJlMzIyZTMwS1Qrc2ZLWlhlM2prTmdTZmcrVXBFOFJzSDRqWE9vNCs3ZVRnSll4UGFpMD0=;" +
+                                                   "MjI0MzVAMzEzNjJlMzIyZTMwQUxkbDZIM2szR1hnY01kQ1JKWGNHRGx0RW9VNjY2dVNHM0h5ZUNHRVQ1ST0=;" +
+                                                   "MjI0MzZAMzEzNjJlMzIyZTMwRzh0YUNvZXJuRERaeEhNbHpFQm1Bcy94Y0NXOE83T0lyZ1dlL211Ny80ND0=;" +
+                                                   "MjI0MzdAMzEzNjJlMzIyZTMwa29SckJqdm1Ybll2ZWNVN0lkLytJcEl4SGpDbnJpYU9ycTlpME5NY0VzND0=;" +
+                                                   "MjI0MzhAMzEzNjJlMzIyZTMwZDJQODFSYzdnTEZQRU42ZGhNMk1LZFY0amVrR0ttZnJKOGlQMU51ckhEND0=";
+    }
+}
